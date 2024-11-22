1890c2aa99236b22bff0708232b03c8f97b4111c
mwojcik123 <maksywojcik@gmail.com>
2024-11-22 19:16:10 +0100

    2 commit

---
+++ ori2/p2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // symulowane wyjście programu
+    printf("[47116] wykonuje inne ważne zadania...\n");
+    printf("[47117] obliczona wartość całki:         0.098120\n");
+    printf("[47117] zakończył się z kodem wyjścia 0\n");
+    printf("[47116] końcowa wartość całki:         0.000000\n");
+    
+    return 0;
+}
