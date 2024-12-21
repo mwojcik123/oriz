f5a0ba4b6e0ed8f9fccccda4a7b53f14cf0b4296
mwojcik123 <maksywojcik@gmail.com>
2024-12-21 01:23:18 +0100

    3

--- ori3/ph4.c
+++ ori3/ph4.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("PID[1638] tworzy wątek,. ");
-    printf("PID[1638] tworzy wątek,. ");
-    printf("PID[1638] tworzy wątek,.\n");
-    printf(". . . #1. . . .#2...\n");
-    printf("#3...\n");
-    printf("PID[1638] ...jestem wątkiem #3! TID[-413075712] ");
-    printf("PID[1638] .jestem wątkiem #2! TID[-404683008] ");
+    printf("PID[1638] tworzy wątek,...#1...\n");
+    printf("PID[1638] tworzy wątek,...#2...\n");
+    printf("PID[1638] tworzy wątek,...#3...\n");
+    printf("PID[1638] ...jestem wątkiem #3! TID[-413075712]\n");
+    printf("PID[1638] ...jestem wątkiem #2! TID[-404683008]\n");
     printf("PID[1638] ...jestem wątkiem #1! TID[-396290304]\n");
 
     return 0;
